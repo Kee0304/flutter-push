53cac1da9166b3b34140393a357eef68bf506912
기남석 <kee0304@naver.com>
2023-04-21 17:48:25 +0900

    test push complete

--- lib/view_model/push_view_model.dart
+++ lib/view_model/push_view_model.dart
@@ -1,40 +1,37 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 class PushViewModel extends ChangeNotifier {
-  final FirebaseMessaging _massaging = FirebaseMessaging.instance;
+  // 메시징 서비스의 기본 인스턴스
+  FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  late NotificationSettings _settings;
+  late String _token;
 
-  // 채널 생성
-  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
-    'high_importance_channel', // id
-    'High Importance Notifications', // title
-    description: 'This channel is used for important notifications.', // description
-    importance: Importance.max,
-  );
-
+  PushViewModel() {
+    onInit();
+  }
 
 
-  Future<void> onInit() async{
-    NotificationSettings settings = await _massaging.requestPermission(
+  Future<void> onInit() async {
+    _settings = await _messaging.requestPermission(
       alert: true,
       announcement: false,
       badge: true,
       carPlay: false,
       criticalAlert: false,
       provisional: false,
-      sound: true
+      sound: true,
     );
-    getToken();
-    onMessage();
-  }
-
-  Future<void> getToken() async {
-    String? token = await _massaging.getToken();
-  }
-
-  Future<void> onMessage() async {
-
-  }
-
 }
+
+
+  }
+
+
+
